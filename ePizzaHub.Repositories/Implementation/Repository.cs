f17ffb18b4d7f3314788682939270478533a9282
esathaiah <esathaiah@US-5CD9288VDN>
2023-08-25 19:28:20 +0530

    Add project files.

---
+++ ePizzaHub.Repositories/Implementation/Repository.cs
@@ -0,0 +1,47 @@
+﻿using ePizzaHub.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePizzaHub.Repositories.Implementation
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        void IRepository<TEntity>.Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<TEntity>.Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        TEntity IRepository<TEntity>.Find(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<TEntity> IRepository<TEntity>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<TEntity>.Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IRepository<TEntity>.SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepository<TEntity>.Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
